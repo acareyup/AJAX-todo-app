82d77f05cbf3f639635e37152b30955b9d6cb57b
Eyup-Acar <acareyup@gmail.com>
2018-09-11 23:07:35 +0300

    install mongodb

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
-var express = require("express");
-var app = express;
+let express = require ("express");
+let app = express(),
+    port = process.env.PORT || 3000,
+    bodyParser = require('body-parser');
+
+var todoRoutes = require('./routes/todos');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/', function (req, res) {
+    res.send("kök dizinden merhaba");
+});
+
+app.use('/api/todos', todoRoutes);
 
 
 
-app.listen(process.env.PORT, function () {
-   console.log("uygulama çalışıyor " + process.env.PORT ); 
+app.listen(port, function () {
+   console.log("uygulama çalışıyor " + port); 
 })
